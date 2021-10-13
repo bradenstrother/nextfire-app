96a351f5b07aca369b3b3e68a29521c9d6ae4186
Braden Strother <bradenstrother@gmail.com>
2021-10-13 14:54:46 -0500

    Removed yarn

---
+++ lib/firebase.ts
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+import 'firebase/compat/analytics';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBz6GorOZEweY3hke_4w2Xo6ExyooGZ5F4",
+  authDomain: "nextfirejs-bagel.firebaseapp.com",
+  projectId: "nextfirejs-bagel",
+  storageBucket: "nextfirejs-bagel.appspot.com",
+  messagingSenderId: "39668228690",
+  appId: "1:39668228690:web:5b87700112bd4a1695758b",
+  measurementId: "G-N2S1EMT5DF"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+export const storage = firebase.storage();
